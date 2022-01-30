f6eabf16ec9a91aa4e48e61f1399715d78a351d8
Julia <abuziarovajulia@gmail.com>
2022-01-30 19:56:09 +0100

    Add user via API

---
+++ app/Http/Controllers/AddUserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AddUserController extends Controller
+{
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $user->remember_token = $token;
+        $user->save();
+
+        return response( $user, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostApiController;
 use App\Http\Controllers\CommentApiController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AddUserController;
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -21,3 +13,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource('posts', PostApiController::class);
 Route::resource('comments', CommentApiController::class);
+
+Route::post('/adduser', [App\Http\Controllers\AddUserController::class, 'register'])->name('user.register');
